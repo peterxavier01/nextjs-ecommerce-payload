66d90db335dd251fff69646cb8c0dd71494c7900
Peter Uadiale <fxavy02@gmail.com>
2024-01-26 15:17:15 +0100

    feat: add promotions component

---
+++ src/app/_components/Promotion/index.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import React, { useEffect, useMemo, useState } from 'react'
+
+import classes from './index.module.scss'
+
+const Promotion = () => {
+  const [time, setTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  })
+
+  const targetDate = new Date()
+  targetDate.setDate(targetDate.getDate() + 3)
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      const currentTime = new Date()
+      const timeDifference = Math.max(Number(targetDate) - Number(currentTime), 0)
+
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+
+      setTime({ days, hours, minutes, seconds })
+
+      if (timeDifference === 0) {
+        clearInterval(timerInterval)
+      }
+    }, 1000)
+
+    return () => {
+      clearInterval(timerInterval)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <section className={classes.promotion}>
+      <div className={classes.textBox}>
+        <h3 className={classes.title}>Deals of the Month</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde hic fugit iusto consequatur
+          doloribus eaque laudantium officiis, quidem vero sint voluptate fuga ratione optio. Enim
+          exercitationem labore facere ab sed!
+        </p>
+
+        <ul className={classes.stats}>
+          <StatBox label="Days" value={time.days} />
+          <StatBox label="Hours" value={time.hours} />
+          <StatBox label="Minutes" value={time.minutes} />
+          <StatBox label="Seconds" value={time.seconds} />
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+const StatBox = ({ label, value }: { label: string; value: number }) => (
+  <li className={classes.statBox}>
+    <h4>{value}</h4>
+    <p>{label}</p>
+  </li>
+)
+
+export default Promotion
